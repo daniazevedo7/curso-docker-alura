17c159f4351a80db23e910ebc16a304eaad0ada7
Daniel Azevedo <d.azevedo.s7@gmail.com>
2023-04-04 21:58:30 -0300

    Modificando

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen("3000", ()=>{
+//CONFIGURAÇÃO PADRÃO
+// app.listen("3000", ()=>{
+//     console.log("Server is listening on port 3000")
+
+//USANDO VARIÁVEL AO INVÉS DA PORTA DIRETO NO CÓDIGO
+app.listen(process.env.PORT, ()=>{
     console.log("Server is listening on port 3000")
 })
